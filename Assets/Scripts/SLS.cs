8dc37aef72d70b18665d285b977a42e1f1a88fbc
Heimer Ahnfelt <thekingofspades@live.se>
2019-01-17 10:56:15 +0100

    Fixed SLS functionality

--- Assets/Scripts/SLS.cs
+++ Assets/Scripts/SLS.cs
@@ -10,8 +10,8 @@ public class SLS : MonoBehaviour
     {
         if (other.tag == "Box")
         {
-            Debug.Log("Box has been delivered");
-            Destroy(other);
+            //Debug.Log("Box has been delivered");
+            Destroy(other.gameObject);
         }
         GameController.instance.BoxDelivered();
     }
