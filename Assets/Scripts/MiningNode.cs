8d6bd688a99d52cb78c44bba8bbb5f1fd9301da1
Heimer Ahnfelt <thekingofspades@live.se>
2019-01-17 10:14:56 +0100

    Created HeimerScene and created MiningNode

---
+++ Assets/Scripts/MiningNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiningNode : MonoBehaviour
+{
+    [SerializeField] private int resourceValue = 5;
+    
+    void OnBoxSpawn()
+    {
+        resourceValue--;
+        if (resourceValue == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
