d3048ef13732194b794718a8ed0068a9cb8df83f
Christoffer Brandt <chrbra1118@skola.goteborg.se>
2019-01-17 14:44:41 +0100

    CameraAI

---
+++ Assets/Scripts/CameraAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAI : MonoBehaviour
+{
+    public string tag = "Player";
+    public float cameraSpeed = 1.0f;
+    public float cameraRotationSpeed = 30.0f;
+    private Vector3 cameraOffset;
+
+    void Start()
+    {
+        Transform player = GameObject.FindGameObjectWithTag(tag).transform;
+        cameraOffset = transform.position - player.position;
+    }
+
+    void Update()
+    {
+        Transform player = GameObject.FindGameObjectWithTag(tag).transform;
+
+        transform.position = player.position + cameraOffset;
+    }
+  
+}
+    
