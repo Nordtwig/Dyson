8897fb60a8d7c454e29b5e2d68dc2c0c77d60300
Heimer Ahnfelt <thekingofspades@live.se>
2019-01-17 10:40:23 +0100

    Started on SLS script and added to HeimerTest

---
+++ Assets/SLS.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SLS : MonoBehaviour
+{
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Box")
+        {
+            Debug.Log("Box has been delivered");
+            Destroy(other);
+        }
+        BoxDelivered
+    }
+}
