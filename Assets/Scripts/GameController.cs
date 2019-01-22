258ad7845c2c85ae9b049edb0996308584a7b3ac
Mabrima <robinkarlola.arkblad@gmail.com>
2019-01-22 13:40:48 +0100

    Skeleton of phase and state setup

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -44,7 +44,7 @@ public class GameController : MonoBehaviour
         MAINMENU,
         GAME,
         PAUSE,
-        END,
+        GAMEOVER,
     }
 
     private void Awake()
@@ -75,7 +75,116 @@ public class GameController : MonoBehaviour
 
     private void Update()
     {
-        PhaseTimer();
+        if (state == GameControllerState.GAME)
+        {
+            //DO THINGS THAT SHOULD BE DONE IN EVERY PHASE
+            PhaseTimer();
+
+            if (currentPhase == 1)
+            {
+                //DO PHASE SPECIFIC THINGS (EG. Set phase specific time, boxAmount, building dyson, etc.)
+            }
+
+            else if (currentPhase == 2)
+            {
+
+            }
+
+            else if (currentPhase == 3)
+            {
+
+            }
+
+            else if (currentPhase == 4)
+            {
+
+            }
+
+            else if (currentPhase == 5)
+            {
+
+            } 
+        }
+
+        else if (state == GameControllerState.MAINMENU)
+        {
+            //DO MAINMENU THINGS
+        }
+
+        else if (state == GameControllerState.PAUSE)
+        {
+            //DO PAUSE THINGS
+        }
+
+        else if (state == GameControllerState.GAMEOVER)
+        {
+            //DO GAME OVER THINGS
+        }
+    }
+
+    public void PhaseTimer()
+    {
+        string timer = String.Format("Time Remaining: {0:0}:{1:00}", (int)totalTimeInPhase / 60, (int)totalTimeInPhase % 60);
+        timeText.text = timer;
+        totalTimeInPhase -= Time.deltaTime;
+
+        if (totalTimeInPhase <= 30)
+        {
+            timeText.color = Color.red;
+        }
+        else
+        {
+            timeText.color = Color.black;
+        }
+
+        if (totalTimeInPhase <= 0)
+        {
+            gameOverText.SetActive(true);
+            //GameOver Condition!
+        }
+    }
+
+    public void BoxDelivered()
+    {
+        boxAmount++;
+        Debug.Log(boxAmount + "/" + phaseAmount + " Boxes delivered");
+        if (boxAmount >= phaseAmount)
+        {
+            StartCoroutine(FindObjectOfType<Sled>().Launch());
+            IncrementPhase();
+        }
+        // Set UI text instead
+    }
+
+    // ============================= PHASE CHANGING STUFF HERE =============================
+    private void IncrementPhase()
+    {
+        boxAmount = 0;
+
+        if(currentPhase == 1)
+        {
+            
+        }
+
+        else if (currentPhase == 2)
+        {
+
+        }
+
+        else if (currentPhase == 3)
+        {
+
+        }
+
+        else if (currentPhase == 4)
+        {
+
+        }
+
+        else if (currentPhase == 5)
+        {
+
+        }
     }
 
     public void Restart()
@@ -86,16 +195,33 @@ public class GameController : MonoBehaviour
         state = GameControllerState.GAME;
     }
 
-    public void BoxDelivered() 
+    public void MainMenu()
     {
-        boxAmount++;
-        Debug.Log(boxAmount + "/" + phaseAmount + " Boxes delivered");
-        if (boxAmount >= phaseAmount)
+        instance.state = GameControllerState.MAINMENU;
+        SceneManager.LoadScene(0);
+    }
+
+    private void DestroyAll()
+    {
+        foreach (PlayerController p in GameObject.FindObjectsOfType<PlayerController>())
         {
-            StartCoroutine(FindObjectOfType<Sled>().Launch());
-            boxAmount = 0;
+            Destroy(p.transform);
         }
-        // Set UI text instead
+
+        foreach (Box b in GameObject.FindObjectsOfType<Box>())
+        {
+            Destroy(b.transform);
+        }
+
+        foreach (MiningRig mr in GameObject.FindObjectsOfType<MiningRig>())
+        {
+            Destroy(mr.transform);
+        }
+
+        foreach (MiningNode mn in GameObject.FindObjectsOfType<MiningNode>())
+        {
+            Destroy(mn.transform);
+        }
     }
 
     public int GetAmountOfDeliveredBoxes()
@@ -103,10 +229,15 @@ public class GameController : MonoBehaviour
         return boxAmount;
     }
 
-    public void MainMenu()
+    // ============================== EXTERNAL TOOLS ===========================
+    public bool GetDebugMode()
     {
-        instance.state = GameControllerState.MAINMENU;
-        SceneManager.LoadScene(0);
+        return debugMode;
+    }
+
+    public void SetDebugMode(bool value)
+    {
+        debugMode = value;
     }
 
     public void DebugSpawnBox()
@@ -131,62 +262,6 @@ public class GameController : MonoBehaviour
             Instantiate(node, player.transform.position + player.transform.TransformDirection(Vector3.forward) * 4, transform.rotation, null);
         else
             player = FindObjectOfType<PlayerController>();
-    }
-
-    private void DestroyAll()
-    {
-        //foreach (Player p in GameObject.FindObjectsOfType<Player>())
-        //{
-        //    Destroy(p);
-        //}
-
-        foreach (Box b in GameObject.FindObjectsOfType<Box>())
-        {
-            Destroy(b);
-        }
-
-        //foreach (MiningRig mr in GameObject.FindObjectsOfType<MiningRig>())
-        //{
-        //    Destroy(mr);
-        //}
-
-        foreach (MiningNode mn in GameObject.FindObjectsOfType<MiningNode>())
-        {
-            Destroy(mn);
-        }
-    }
-
-    public bool GetDebugMode()
-    {
-        return debugMode;
-    }
-
-    public void SetDebugMode(bool value)
-    {
-        debugMode = value;
-    }
-
-    public void PhaseTimer()
-    {
-        
-        string timer = String.Format("Time Remaining: {0:0}:{1:00}", (int)totalTimeInPhase / 60, (int)totalTimeInPhase % 60);
-        timeText.text = timer;
-        totalTimeInPhase -= Time.deltaTime;
-
-        if (totalTimeInPhase <= 30)
-        {
-            timeText.color = Color.red;
-        }
-        else
-        {
-            timeText.color = Color.black;
-        }
-
-        if (totalTimeInPhase <= 0)
-        {
-            gameOverText.SetActive(true);    
-            //GameOver Condition!
-        }
     }
 
 }
