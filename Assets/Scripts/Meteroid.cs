c50b84879e03b554a4ece557653c25d4d11473e4
Heimer Ahnfelt <thekingofspades@live.se>
2019-01-23 15:10:24 +0100

    Added danger zone to meteorites

--- Assets/Scripts/Meteroid.cs
+++ Assets/Scripts/Meteroid.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class Meteroid : MonoBehaviour
 {
+    [SerializeField] GameObject dangerZone;
+    private GameObject zone;
+
+    private void Start()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit))
+        {
+            zone = Instantiate(dangerZone, hit.point, Quaternion.identity);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject.transform.parent.gameObject);
+        Destroy(zone);
+        Destroy(gameObject.transform.parent.gameObject);        
     }
 }
