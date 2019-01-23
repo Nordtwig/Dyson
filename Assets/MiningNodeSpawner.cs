1f3aeb84ba297a1a2284eeb2857353c6ab0c1520
Heimer Ahnfelt <thekingofspades@live.se>
2019-01-23 14:02:21 +0100

    Created NodeSpawner script and object in HeimerScene

---
+++ Assets/MiningNodeSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script creator: Heimer
+/// </summary>
+
+public class MiningNodeSpawner : MonoBehaviour
+{
+    [Tooltip("Amount of Nodes generated during startup")]
+    [SerializeField] int initialSpawnCount = 10;
+    [Tooltip("miningNodeCenterpoint prefab goes here")]
+    [SerializeField] GameObject miningNode;
+
+
+    void Start()
+    {
+        SpawnNode(initialSpawnCount);
+    }
+
+    //This function spawns x amount of nodes with random positions around the asteroid.
+    void SpawnNode(int spawnCount)
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            Instantiate(miningNode, transform.position, Random.rotation);
+        }
+    }
+}
