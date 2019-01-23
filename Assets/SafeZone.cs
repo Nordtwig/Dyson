92cc1b5d14050b0cbbe995025927fac45fcdc1eb
Mabrima <robinkarlola.arkblad@gmail.com>
2019-01-23 14:29:22 +0100

    Added sanctuary, no Nodes or Meteors will be allowed inside

---
+++ Assets/SafeZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeZone : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Meteroid" || other.tag == "Node")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
