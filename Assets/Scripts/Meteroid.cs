7d25a2535bd895a37fb497d8f1b66ed9a7d4ff98
Petter Svensson <petter.svensson.90@gmail.com>
2019-01-23 15:03:28 +0100

    Added node spawn on meteor impact
    Changes to Meteroid Prefab and Meteroid Scripts

--- Assets/Scripts/Meteroid.cs
+++ Assets/Scripts/Meteroid.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 
 public class Meteroid : MonoBehaviour
 {
+    [SerializeField] GameObject miningNode;
+    private Quaternion miningNodeSpawnRotation;
+    [SerializeField] float randomNodeSpawnChance;
+    private float spawnValue;
+
+    //If meteroid collides with asteroid, spawns a miningnode according to percentage
     private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Asteroid")
+        {
+            spawnValue = Random.Range(0f, 1f);
+            if (spawnValue <= randomNodeSpawnChance)
+            {
+                miningNodeSpawnRotation = gameObject.GetComponentInParent<Transform>().rotation;
+                Instantiate(miningNode, miningNode.transform.position, miningNodeSpawnRotation);
+            }
+            Invoke("DestroyMeteroid", 0.2f);
+        }
+    }
+
+    private void DestroyMeteroid()
     {
         Destroy(gameObject.transform.parent.gameObject);
     }
