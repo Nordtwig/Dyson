3705dd0cf624c78549b219539ca3ca339ddfe2c4
Ulrik <ulrik.bergstrom@gmail.com>
2019-01-24 14:50:53 +0100

    Changed sled script

--- Assets/Scripts/Sled.cs
+++ Assets/Scripts/Sled.cs
@@ -24,7 +24,6 @@ public class Sled : MonoBehaviour
         sled = FindObjectOfType<Sled>();
         sledStartPosition = sled.transform.position;
         sledStartRotation = sled.transform.rotation;
-		launchText = GameObject.Find("LaunchTimerText").GetComponent<Text>();
 	}
 
 	public void StartLaunchCo()
