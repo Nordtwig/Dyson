2296c7b1b1b70b9bd3670692f2ab6a27243df055
Christoffer Brandt <chrbra1118@skola.goteborg.se>
2019-01-25 13:27:56 +0100

    Mining Rig Eject

---
+++ Assets/Scripts/EjectBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjectBox : MonoBehaviour
+{
+    public GameObject BoxEjected;
+    public Transform SpawnpointBox;
+
+    void Start()
+    {
+        Instantiate(BoxEjected, SpawnpointBox.position, SpawnpointBox.rotation);
+        Destroy(BoxEjected, 1f);
+    }
+
+    void Update()
+    {
+        
+    }
+}
