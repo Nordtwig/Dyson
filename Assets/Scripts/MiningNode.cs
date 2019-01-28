e3ac61d86b681e42797feeaa8f1f38b6f0e0d42e
Ulrik <ulrik.bergstrom@gmail.com>
2019-01-28 15:19:40 +0100

    Added state for different metals

--- Assets/Scripts/MiningNode.cs
+++ Assets/Scripts/MiningNode.cs
@@ -3,16 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-///  Created by Heimer, modified by Robin
+///  Created by Heimer, modified by Robin, Ulrik
 /// </summary>
 
 public class MiningNode : MonoBehaviour
 {
-    public int resourceValue = 5;
+	public int resourceValue;
+	int metalRandom;
 
-    public bool OnBoxSpawn()
+	// States
+	public MetalVarieties state = MetalVarieties.NULL;
+
+	public enum MetalVarieties
+	{
+		NULL = 1,
+		GOLD,
+		SILVER,
+		COPPER,
+	}
+
+	public void Start()
+	{
+		metalRandom = Random.Range(1, 10);
+
+		if (metalRandom < 2)
+		{
+			state = MetalVarieties.GOLD;
+		}
+		else if (metalRandom < 5)
+		{
+			state = MetalVarieties.SILVER;
+		}
+		else if (metalRandom < 11)
+		{
+			state = MetalVarieties.COPPER;
+		}
+	}
+
+	public void randomForMetal()
+	{
+
+	}
+
+	public bool OnBoxSpawn()
     {
-        resourceValue--;
+		resourceValue = Random.Range(1, 5);
+		resourceValue--;
         if (resourceValue == 0)
         {
             Destroy(gameObject);
