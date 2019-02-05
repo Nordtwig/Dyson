ef2a79034af32678f144c3e7245428a3d12e321d
Noah <nordqvist.gaming@gmail.com>
2019-02-05 13:56:38 +0100

    MissionPresets updates  Current Mission

--- Assets/Scripts/MissionPresets.cs
+++ Assets/Scripts/MissionPresets.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Created by: Noah Nordqvist
@@ -18,18 +19,22 @@ public class MissionPresets : MonoBehaviour
     public int playerCreditsHard;
     public PhaseSpecifics[] phaseSpecificsHard;
 
+    public Text currentMission;
 
     public void Easy() {
+        currentMission.text = "Current Mission: Easy";
+        GameController.instance.phaseSpecifics = new PhaseSpecifics[phaseSpecificsEasy.Length];
+        GameController.instance.playerCredits = playerCreditsEasy;
         for (int i = 0; i < phaseSpecificsEasy.Length; i++) {
-            GameController.instance.playerCredits = playerCreditsEasy;
             GameController.instance.phaseSpecifics[i].totalTimeInPhase = phaseSpecificsEasy[i].totalTimeInPhase;
             GameController.instance.phaseSpecifics[i].phaseBoxAmount = phaseSpecificsEasy[i].phaseBoxAmount;
             GameController.instance.phaseSpecifics[i].timeBetweenMeteroids = phaseSpecificsEasy[i].timeBetweenMeteroids;
         }
     }
     public void Medium() {
-            GameController.instance.phaseSpecifics = new PhaseSpecifics[phaseSpecificsMedium.Length];
-            GameController.instance.playerCredits = playerCreditsMedium;
+        currentMission.text = "Current Mission: Medium";
+        GameController.instance.phaseSpecifics = new PhaseSpecifics[phaseSpecificsMedium.Length];
+        GameController.instance.playerCredits = playerCreditsMedium;
         for (int i = 0; i < phaseSpecificsMedium.Length; i++) {
             GameController.instance.phaseSpecifics[i] = new PhaseSpecifics();
             GameController.instance.phaseSpecifics[i].totalTimeInPhase = phaseSpecificsMedium[i].totalTimeInPhase;
@@ -38,8 +43,10 @@ public class MissionPresets : MonoBehaviour
         }
     }
     public void Hard() {
+        currentMission.text = "Current Mission: Hard";
+        GameController.instance.phaseSpecifics = new PhaseSpecifics[phaseSpecificsHard.Length];
+        GameController.instance.playerCredits = playerCreditsHard;
         for (int i = 0; i < phaseSpecificsHard.Length; i++) {
-            GameController.instance.playerCredits = playerCreditsHard;
             GameController.instance.phaseSpecifics[i].totalTimeInPhase = phaseSpecificsHard[i].totalTimeInPhase;
             GameController.instance.phaseSpecifics[i].phaseBoxAmount = phaseSpecificsHard[i].phaseBoxAmount;
             GameController.instance.phaseSpecifics[i].timeBetweenMeteroids = phaseSpecificsHard[i].timeBetweenMeteroids;
