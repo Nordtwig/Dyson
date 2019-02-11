217558f792323248dfc656b6c15ebfa3d12cb7b9
Heimer Ahnfelt <thekingofspades@live.se>
2019-02-11 15:48:35 +0100

    Added Music files and started on music manager

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private AudioSource musicIntro;
+    private AudioSource musicLoop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource[] audios = GetComponents<AudioSource>();
+        musicIntro = audios[0];
+        musicLoop = audios[1];
+        musicIntro.Play();
+        musicLoop.PlayDelayed(musicIntro.clip.length);
+    }
+
+}
