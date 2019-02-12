9f389fb0052d50e54a3526c35e1d8f4bc89ba69e
Petter Svensson <petter.svensson.90@gmail.com>
2019-02-12 11:52:38 +0100

    Added MiniMap. Drag in MiniMap Prefab to your canvas

---
+++ Assets/Scripts/MiniMapIcon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapIcon : MonoBehaviour
+{
+
+    MiningRig miningRig;
+    Material material;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        miningRig = transform.parent.gameObject.GetComponent<MiningRig>();
+        material = gameObject.GetComponent<MeshRenderer>().material;
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        if (miningRig.functioning && miningRig.coBoxSpawnRunning)
+        {
+            material.color = Color.green;
+        }
+        else if (miningRig.functioning && !miningRig.coBoxSpawnRunning)
+        {
+            material.color = Color.red;
+        }
+        else
+        {
+            material.color = Color.yellow;
+        }
+    }
+}
