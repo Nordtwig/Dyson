598b91f9afed52f0610f6515ce009d01cf31e854
Mabrima <robinkarlola.arkblad@gmail.com>
2019-02-19 09:55:32 +0100

    Dyson sphere ingame

--- Assets/Scripts/ObjectScripts/Hexes.cs
+++ Assets/Scripts/ObjectScripts/Hexes.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Hexes : MonoBehaviour
 {
-    private GameController gameController;
     public GameObject Phase2Hexes;
     public GameObject Phase3Hexes;
     public GameObject Phase4Hexes;
@@ -12,38 +11,31 @@ public class Hexes : MonoBehaviour
 
     void Start()
     {
-        gameController = FindObjectOfType<GameController>();
         Phase2Hexes.SetActive(false);
         Phase3Hexes.SetActive(false);
         Phase4Hexes.SetActive(false);
         Phase5Hexes.SetActive(false);
     }
 
-    void Update()
+    public void UpdateHexes()
     {
-        if (gameController.currentPhase == 2)
+        if (GameController.instance.currentPhase == 2)
         {
             Phase2Hexes.SetActive(true);
         }
 
-        if (gameController.currentPhase == 3)
+        if (GameController.instance.currentPhase == 3)
         {
-            Phase2Hexes.SetActive(true);
             Phase3Hexes.SetActive(true);
         }
 
-        if (gameController.currentPhase == 4)
+        if (GameController.instance.currentPhase == 4)
         {
-            Phase2Hexes.SetActive(true);
-            Phase3Hexes.SetActive(true);
             Phase4Hexes.SetActive(true);
         }
 
-        if (gameController.currentPhase == 5)
+        if (GameController.instance.currentPhase == 5)
         {
-            Phase2Hexes.SetActive(true);
-            Phase3Hexes.SetActive(true);
-            Phase4Hexes.SetActive(true);
             Phase5Hexes.SetActive(true);
         }
     }
