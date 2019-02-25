12d8671d9b7134b4de1642c748660198db41d794
Mabrima <robinkarlola.arkblad@gmail.com>
2019-02-25 14:09:34 +0100

    Test Lerp

--- Assets/Scripts/ObjectScripts/CameraFollow.cs
+++ Assets/Scripts/ObjectScripts/CameraFollow.cs
@@ -14,10 +14,8 @@ public class CameraFollow : MonoBehaviour
     private Transform rotX;
 
     public float speedH = 2.0f;
-    public float speedV = 2.0f;
 
     private float yaw = 0.0f;
-    private float pitch = 0.0f;
 
     Vector3 offset;
 
@@ -37,10 +35,11 @@ public class CameraFollow : MonoBehaviour
         Vector3 v3 = transform.position - asteroid.position;
         transform.rotation = Quaternion.FromToRotation(transform.up, v3) * transform.rotation;
 
-        if (GameController.instance.state == GameController.GameControllerState.GAME) yaw += speedH * Input.GetAxis("Mouse X");
-        pitch -= speedV * Input.GetAxis("Mouse Y");
+        if (GameController.instance.state == GameController.GameControllerState.GAME)
+            yaw = (yaw + speedH * Input.GetAxis("Mouse X"));
+        //pitch -= speedV * Input.GetAxis("Mouse Y");
 
-        //rotY.localEulerAngles = new Vector3(pitch, 0.0f, 0.0f);
-        rotX.localEulerAngles = new Vector3(0.0f, yaw, 0.0f);
+        rotX.localRotation = Quaternion.Lerp(rotX.localRotation, Quaternion.Euler(new Vector3(0.0f, yaw, 0.0f)), 0.1f);
+
     }
 }
